72f56d0006925d483ced5930c0776d158ffb61bd
dmpeterson317 <dmpeterson317@gmail.com>
2019-02-20 12:34:58 -0500

    null

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,24 @@
 import helper
 
 def main():
+	# assert("stunt nuts")
 
 	userInput = input("Enter one or more sentences: ")
 
 	text = helper.clean_text(userInput)
+	
 	if helper.isPalindrome(text): 
 		print("is a palindrome")
-	else:
+	else: 
 		print("is not a palindrome")
+
+
+	# alternative method of evaluating boolean
+	# resultVariable = helper.isPalindrome(text)
+	# if resultVariable == True:
+	# 	print("is a palindrome")
+	# else:
+	# 	print("is not a palindrome")
 
 if __name__ == "__main__":
 	main()
